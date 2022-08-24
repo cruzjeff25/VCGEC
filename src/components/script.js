77bb50b08f20269bc051c067df9e67cf1eb54ca5
Jeferson <jefbandong@gmail.com>
2022-08-24 22:41:25 +0800

    first commit

---
+++ src/components/script.js
@@ -0,0 +1,29 @@
+
+
+var modal = document.getElementById("myModal");
+
+
+var img = document.getElementById("myImg");
+var img1 = document.getElementById("myImg1");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+img1.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
