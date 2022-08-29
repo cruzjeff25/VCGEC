f6599684828534e69cc1d4de904caa4fe89df496
Jeferson <jefbandong@gmail.com>
2022-08-29 21:13:33 +0800

    first commit

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,41 +1,58 @@
 import React from 'react'
 import form from '../img/form.png'
-// import ScriptTag from 'react-scripts-tag'
+
 const Forms = () => {
   return (
-    <div className="sub-con">
-		<div className="requirements">
+
+	<div className="body">
+
+		 <div className="requirements">
 			<h2>Loan requirements</h2>
 			<ul>
 				<li>&#10003; Lorem ipsum dolor sit amet consectetur adipisicing.</li>
 				<li>&#10003; Lorem ipsum dolor sit.</li>
 				<li>&#10003; Lorem ipsum dolor sit amet.</li>
 			</ul>
+
+		 </div>
+		<h2 className="name">Regular Loan </h2>
+		<div className="con-form" >			
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 1</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />
+				</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 2</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />
+				</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 3</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />	
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 4</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 5</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+        </div> 
+		<h2 className="name">Special Loan </h2>
+		<div className="con-form" >			
+			<div className="form" >
+				<h2 className="form-name" id="form-name">FORM 1</h2>
+				<a href="../apk.txt" class="download" download>Download</a>
+				<img src={form} alt="form" />
+				</div>
+			<div className="form" ></div>
 		</div>
-				<div className="con-form" >
-					
-						<div className="form" >
-								<h2 className="form-name" id="form-name">FORM 1</h2>
-								<a href="../apk.txt" class="download" download>Download</a>
-								<img src={form} alt="form" />
-						</div>
-						<div className="form" >
-								<h2 className="form-name" id="form-name">FORM 2</h2>
-								<a href="../apk.txt" class="download" download>Download</a>
-								<img src={form} alt="form" />
-						</div>
-						<div className="form" >
-						<h2 className="form-name" id="form-name">FORM 3</h2>
-								<a href="../apk.txt" class="download" download>Download</a>
-								<img src={form} alt="form" />	
-						</div>
-						<div className="form" >
-								<h2 className="form-name" id="form-name">FORM 4</h2>
-								<a href="../apk.txt" class="download" download>Download</a>
-								<img src={form} alt="form" />
-						</div>
-        </div>
-   </div>
+	</div>
     
 
   )
