375580cf5b32d7079ce5ea35fc70850caa6863c1
cruzjeff25 <cruzjeff254@gmail.com>
2022-09-13 14:38:00 +0800

    red

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import form from '../img/form.png'
+import pdf from '../documents/Form.pdf'
 
 const Forms = () => {
   return (
@@ -9,45 +10,71 @@ const Forms = () => {
 		 <div className="requirements">
 			<h2>Loan requirements</h2>
 			<ul>
-				<li>&#10003; Lorem ipsum dolor sit amet consectetur adipisicing.</li>
-				<li>&#10003; Lorem ipsum dolor sit.</li>
-				<li>&#10003; Lorem ipsum dolor sit amet.</li>
+				<li> Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+				<li> Lorem ipsum dolor sit.</li>
+				<li> Lorem ipsum dolor sit amet.</li>
 			</ul>
 
 		 </div>
 		<h2 className="name">Regular Loan </h2>
 		<div className="con-form" >			
 			<div className="form" >
-				<h2 className="form-name" id="form-name">FORM 1</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<h2 className="form-name" id="form-name">Productivity Loan</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
 				<img src={form} alt="form" />
 				</div>
 			<div className="form" >
-				<h2 className="form-name" id="form-name">FORM 2</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<h2 className="form-name" id="form-name">Educational Loan</h2>
+				<a href={pdf} target="_blank" class="download" download>Download</a>
 				<img src={form} alt="form" />
 				</div>
 			<div className="form" >
-				<h2 className="form-name" id="form-name">FORM 3</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<h2 className="form-name" id="form-name">Travel Loan</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
 				<img src={form} alt="form" />	
 			</div>
 			<div className="form" >
-				<h2 className="form-name" id="form-name">FORM 4</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<h2 className="form-name" id="form-name">MIGS</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
 				<img src={form} alt="form" />
 			</div>
 			<div className="form" >
-				<h2 className="form-name" id="form-name">FORM 5</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<h2 className="form-name" id="form-name">1 COOP</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
 				<img src={form} alt="form" />
 			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">Commodity Loan</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">Emergency/Calamity</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">Rediscounting</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">Promo</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+			<div className="form" >
+				<h2 className="form-name" id="form-name">Motorcycle</h2>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
+				<img src={form} alt="form" />
+			</div>
+
         </div> 
 		<h2 className="name">Special Loan </h2>
 		<div className="con-form" >			
 			<div className="form" >
 				<h2 className="form-name" id="form-name">FORM 1</h2>
-				<a href="../apk.txt" class="download" download>Download</a>
+				<a href={pdf} target='_blank' class="download" download>Download</a>
 				<img src={form} alt="form" />
 				</div>
 			<div className="form" ></div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,27 +5,54 @@ function Home() {
   return (
     <>
       <div className="con-home" id='Home'>
-        <div className='container'>
-          <div className='con-text'>
-            <h2 >Valenzuela City Government Employees Cooperative</h2>
-            <p>"Stable and progressive life towards abundancy"</p>  
-            <button><a href='#section2'>Join Now!</a></button>
+
+          <div className='container'>
+            <div className='col1'>
+              <h2 >Valenzuela City Government Employees Cooperative</h2>
+              <p>"Stable and progressive life towards abundance"</p>  
+              <button><a href='#section3'>Join Now!</a></button>
+            </div>
+
           </div>
-        </div>
-        <div className='con-img'>
-        
-          <img src={homeImage} alt='savings'/>
-          <br></br>
-          <p>All <span>Valenzuela City Government Employees</span> are qualified to be members</p>
-        
-        </div>
+
+          <div className='container'>
+            <div className='col2'>
+              <img src={homeImage} alt='savings'/>
+              <br></br>
+              <p>All <span>Valenzuela City Government Employees</span> are qualified to be members</p>
+            </div>
+          </div>
+
+          <div className='sec2-nav'><a href='#section2'>&darr; Services Offered &darr;</a></div>
+
       </div>
 
+      <section id='section2'>
+          <h3 className='section2-title'>Services Offered</h3>
+          <div>
+            <h4>Regular Loans</h4>
+            <ul>
+              <li>Productivity Loan</li>
+              <li>Educational Loan</li>
+              <li>Travel Loan</li>
+              <li>MIGS</li>
+              <li>1 COOP</li>
+              <li>Commodity Loan</li>
+              <li>Emergency/Calamity Loan</li>
+              <li>Rediscounting</li>
+              <li>Promo</li>
+              <li>Motorcycle</li>
+            </ul>
+          </div>
 
-         
-        <section id='section2'>
-          section3
+          
         </section>
+         
+      <section id='section3'>
+        <h3>VCGEC Membership</h3>
+        <h4>Be a member now for as low as <span>&#8369;850</span></h4>
+
+      </section>
     </>
 
 
