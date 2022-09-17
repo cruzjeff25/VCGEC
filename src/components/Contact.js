cdd461d1d6d4e946640b4e5518484da83a1deb2e
cruzjeff25 <cruzjeff254@gmail.com>
2022-09-17 22:41:19 +0800

    emailjs contactform

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,32 @@
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+  const form = useRef();
+
+  const [name,setName] = useState();
+  const [email,setEmail] = useState();
+  const [message,setMessage] = useState();
+  const [error,setError] = useState(false);
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    if(name == null || name.length == 0 || email == null || email.length == 0 ||  message == null || message.length == 0 ){
+      setError(true)
+    }else{
+    emailjs.sendForm('gml', 'templara', form.current, 'wPACs5b')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      alert('Message Sent');
+      e.target.reset();
+    }
+  };
+
   return (
     <div className="contact">
       <section className='contact-sec'>
@@ -10,14 +37,17 @@ const Contact = () => {
         </p>
       </div>
 
-      <form className="contact-form">
+      <form className="contact-form" ref={form} onSubmit={sendEmail}>
         <div className="contact-subcon">
+
           <div className="con-input">
-            <input className="form-name" type="text" name="name" placeholder="Your Name*" />
-            <input  className="form-email" type="email" name="email" placeholder="Your Email*" />
+            <input onChange={e=>setName(e.target.value)} className="form-name" type="text" name="name"  />
+    
+            <input onChange={e=>setEmail(e.target.value)} className="form-email" type="email" name="email" />
           </div>
 
-          <textarea placeholder="Your Message*"></textarea>
+          <textarea onChange={e=>setMessage(e.target.value)} name="message" placeholder="Your Message*"></textarea>
+          {error ? <label>*Inputs can't be Empty*</label> : " " }
         </div>
         <div className="con-button">
         <button type="submit">Submit</button>
@@ -29,4 +59,4 @@ const Contact = () => {
   )
 }
 
-export default Contact
+export default Contact;
