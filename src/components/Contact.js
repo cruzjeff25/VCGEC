a9a42b4116c555995d0debebb742bd74efa6bc42
cruzjeff25 <cruzjeff254@gmail.com>
2022-09-18 16:37:26 +0800

    contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,7 @@
 import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { FaFacebookSquare, FaEnvelope, FaPhoneAlt, FaSearchLocation } from "react-icons/fa";
+
 
 const Contact = () => {
   const form = useRef();
@@ -13,10 +15,10 @@ const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    if(name == null || name.length == 0 || email == null || email.length == 0 ||  message == null || message.length == 0 ){
+    if(name == null || name.length === 0 || email === null || email.length === 0 ||  message === null || message.length === 0 ){
       setError(true)
     }else{
-    emailjs.sendForm('gml', 'templara', form.current, 'wPACs5b')
+    emailjs.sendForm('gmail', 'template_1rzzzra', form.current, 'wPYiuptMZmAPACs5b')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -29,31 +31,46 @@ const Contact = () => {
 
   return (
     <div className="contact">
-      <section className='contact-sec'>
-      <div className="contact-title">
-        <h2>Contact <span>Us</span></h2>
-        <p>
-          Please get in touch and our contact support team will answer all your questions or suggestions.
-        </p>
+
+
+      <div className='con-left'>
+        <h2>Get in Touch</h2>
+        <p>Visit or Contact Us</p>
+
+        <ul>
+          <li><FaSearchLocation color='#0272ce' size="1.2rem"/><a>A. Pablo St. Malinta, District I Valenzuela City</a></li>
+          <li><FaPhoneAlt color='#0272ce' size="1.2rem"/><a>700-22919 | 352-1000 1815</a></li>
+          <li><FaEnvelope color='#0272ce' size="1.2rem"/><a href = "mailto: vcgec@yahoo.com" target="_blank">vcgec@yahoo.com</a></li>
+          <li><FaFacebookSquare color='#0272ce' size="1.2rem"/><a href='https://www.facebook.com/vcge.coop' target="_blank">facebook.com/vcge.coop</a></li>
+        </ul>
+
       </div>
 
-      <form className="contact-form" ref={form} onSubmit={sendEmail}>
-        <div className="contact-subcon">
+        <div className='con-right'>
+          <section className='contact-sec'>
+            <div className="contact-title">
+              <h2>Comments & Suggestions</h2>
+            </div>
 
-          <div className="con-input">
-            <input onChange={e=>setName(e.target.value)} className="form-name" type="text" name="name"  />
-    
-            <input onChange={e=>setEmail(e.target.value)} className="form-email" type="email" name="email" />
-          </div>
+            <form className="contact-form" ref={form} onSubmit={sendEmail}>
+              <div className="contact-subcon">
 
-          <textarea onChange={e=>setMessage(e.target.value)} name="message" placeholder="Your Message*"></textarea>
-          {error ? <label>*Inputs can't be Empty*</label> : " " }
+                <div className="con-input">
+                  <input onChange={e=>setName(e.target.value)} className="form-name" type="text" name="name" placeholder="Name" />
+          
+                  <input onChange={e=>setEmail(e.target.value)} className="form-email" type="email" name="email" placeholder="Email"/>
+                </div>
+
+                <textarea onChange={e=>setMessage(e.target.value)} name="message" placeholder="Your Message"></textarea>
+                {error ? <label>Inputs can't be Empty.</label> : " " }
+              </div>
+              <div className="con-button">
+              <button type="submit">Submit</button>
+              </div>
+            </form>   
+          </section>  
         </div>
-        <div className="con-button">
-        <button type="submit">Submit</button>
-        </div>
-      </form>
-  </section>
+
       
     </div>
   )
